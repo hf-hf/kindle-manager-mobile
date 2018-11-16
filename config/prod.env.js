f6f072276983b8f13700c4e86fa91f99f4573f00
hf-hf <553527481@qq.com>
2018-11-16 17:08:00 +0800

    修改使用https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://m.hunfan.top/web-api/"',//"http://m.hunfan.top/web-api/",
+  BASE_API: '"https://m.hunfan.top/web-api/"',//"http://m.hunfan.top/web-api/",
   ROUTER_BASE: '"/mobile/"'
 }
